c012f46d298288ffa28a656c9ab9fc764f89e8f4
Peter Borkuti <peter.borkuti@liferay.com>
2015-10-12 15:56:27 +0200

    fps

--- core/src/hu/bp/conway/ConwaysGame.java
+++ core/src/hu/bp/conway/ConwaysGame.java
@@ -11,15 +11,18 @@ import java.util.concurrent.Executors;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 public class ConwaysGame extends ApplicationAdapter {
-	static final int NUM_OF_THREADS = 2;
+	static final int NUM_OF_THREADS = 10;
 	public static final int WIDTH = 20000;
 	public static final int HEIGHT = 20000;
 
@@ -34,6 +37,8 @@ public class ConwaysGame extends ApplicationAdapter {
 	CameraEvents mouse;
 	private boolean camWasReset = false;
 	private boolean camWasSet = false;
+	private BitmapFont font;
+	private SpriteBatch batch;
 
 	private void resetCamera() {
 		camera = new OrthographicCamera(WIDTH, HEIGHT);
@@ -44,13 +49,18 @@ public class ConwaysGame extends ApplicationAdapter {
 
 	@Override
 	public void create() {
+		font = new BitmapFont();
+		font.setColor(Color.BLUE);
+		font.getData().setScale(3);
+
+		batch = new SpriteBatch(); 
 		camera = new OrthographicCamera(WIDTH, HEIGHT);
 		mouse = new CameraEvents(camera);
 
 		resetCamera();
 
 		renderer = new ShapeRenderer();
-		universe = new Universe(UNIVERSE_N, 0.3f);
+		universe = new Universe(UNIVERSE_N, 0.5f);
 
 		executor = Executors.newFixedThreadPool(NUM_OF_THREADS);
 		coordinator = new Coordinator(universe, executor, NUM_OF_THREADS, 1);
@@ -74,9 +84,16 @@ public class ConwaysGame extends ApplicationAdapter {
 		renderer.setProjectionMatrix(camera.combined);
 
 		Drawer.draw(camera, renderer, coordinator.out);
+		drawFPS();
 
 		coordinator.oneStep();
 		coordinator.swap();
+	}
+
+	private void drawFPS() {
+		batch.begin();
+		font.draw(batch, "" + Gdx.graphics.getFramesPerSecond(), 0, font.getLineHeight());
+		batch.end();
 	}
 
 	public void renderOld() {
